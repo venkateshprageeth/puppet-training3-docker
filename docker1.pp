df921f65815145e0704b3382e21ff08560e93492
root <root@jenkins-master.us-central1-a.c.devops-training-245521.internal>
2019-10-13 16:47:49 +0000

    fix

--- docker1.pp
+++ docker1.pp
@@ -7,12 +7,13 @@ require => Package['docker'],
 }
 exec { 'rm':
 command => '/bin/docker rm -f $(sudo docker ps -a -q)',
+before => Exec['build'],
 }
 exec { 'build':
 command => '/bin/docker build . -t website',
-require => Exec['rm']
 }
 exec { 'run':
 command => '/bin/docker run -it -d -p 82:80 -d website',
+require => Exec['build'],
 }
 
